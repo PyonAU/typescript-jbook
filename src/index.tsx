7d4d7f2605d14360211ded94257790b62e05369f
Pyon18Pyon <ayasayshi@gmail.com>
2022-11-11 16:56:28 +1100

    feat: initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState, useEffect, useRef, Fragment } from 'react';
+import { createRoot } from 'react-dom/client';
+import * as esbuild from 'esbuild-wasm';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container!); // We need to add ! because we are using typeScript.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  // State
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState(''); 
+
+  // Ref
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm' // This is to access web assembly binary from the public directory.
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const clickHandler = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  };
+
+  return (
+    <Fragment>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </Fragment>
+  );
+};
+
+root.render(<App />);
