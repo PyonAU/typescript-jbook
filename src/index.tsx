61c78920340d7fbb7391dc1ba1653f3e7ab386bc
Pyon18Pyon <ayasayshi@gmail.com>
2023-03-29 18:15:31 +1100

    feat: safety handling untrusted code execution

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
-import { useState, useEffect, useRef, Fragment } from 'react';
+import React, { useState, useEffect, useRef, Fragment } from 'react';
 import { createRoot } from 'react-dom/client';
 import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // We need to add ! because we are using typeScript.
@@ -8,15 +10,15 @@ const root = createRoot(container!); // We need to add ! because we are using ty
 const App = () => {
   // State
   const [input, setInput] = useState('');
-  const [code, setCode] = useState(''); 
 
   // Ref
   const ref = useRef<any>();
+  const iframe = useRef<any>();
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm' // This is to access web assembly binary from the public directory.
+      wasmURL: 'http://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm', // This is to access web assembly binary from the public directory.
     });
   };
 
@@ -29,21 +31,56 @@ const App = () => {
       return;
     }
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
+    iframe.current.srcdoc = html;
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
-    setCode(result.code);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              // throw(err);
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <Fragment>
-      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <textarea
+        value={input}
+        rows={4} 
+        cols={50}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
         <button onClick={clickHandler}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </Fragment>
   );
 };
